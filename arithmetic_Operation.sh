8c13d8769c1126ce1b168512af17f5ea68847819
Ankita <Ankitaangane20@gmail.com>
2020-10-16 20:32:01 +0530

    Added second arithmetic operation

--- arithmetic_Operation.sh
+++ arithmetic_Operation.sh
@@ -10,6 +10,11 @@ read -p "Enter Third Number : " number3
 
 echo "Your numbers are : $number1 $number2 $number3"
 
+#First Computation
 operation1=$(( number1 + ( number2 * number3 )))
-
 echo "number1 + number2 * number3 : $operation1"
+
+#Second Computation
+operation2=$(( ( number1 * number2 ) + number3 ))
+echo "number1 * number2 + number3 : $operation2"
+
