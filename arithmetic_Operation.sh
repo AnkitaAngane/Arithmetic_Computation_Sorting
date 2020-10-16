dd40806c8c3df28ebcf0019544cffb98f6f6df51
Ankita <Ankitaangane20@gmail.com>
2020-10-16 19:33:29 +0530

    Added input from user for numbers

---
+++ arithmetic_Operation.sh
@@ -0,0 +1,11 @@
+#! /bin/bash
+
+#Author : Ankita Angane
+#Arithmetic Operations And Sorting Program
+
+#UC1:Input three numbers
+read -p "Enter First Number : " number1
+read -p "Enter Second Number : " number2
+read -p "Enter Third Number : " number3
+
+echo "Your numbers are : $number1 $number2 $number3"
